f3fbcbcb8700350fe45ca6da88cca18026ec878b
jotaeme890 <permanentjavi@gmail.com>
2023-01-13 09:12:02 +0100

    Excepcion

---
+++ ExcepcionApareamientoImposible.java
@@ -0,0 +1,5 @@
+public class ExcepcionApareamientoImposible extends Exception{
+    public ExcepcionApareamientoImposible(){
+        System.out.println("Es imposible crear un gatito siendo del mismo sexo");
+    }
+}
